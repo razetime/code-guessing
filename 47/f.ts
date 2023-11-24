fb6f8f36e23a94a523879f4a2551e8b4382292d3
razetime <rraghu.11502@gmail,com>
2023-11-24 11:39:48 +0800

    47

---
+++ 47/f.ts
@@ -0,0 +1,24 @@
+let f1=function(F1:number[]):number[]{let f2:number[][]=[];
+let F2: number[]=[];for(let f3:number=0;f3<F1.length;f3++){
+;const                                                  F3:
+number                                                    =
+F1[f3];
+let f4                                    :
+number                                   []
+=[];for(let f6=0;f6<f3;f6++){let F4=f2[f6];
+var f5=F1[f6];if(f5<F3&&F4.length>f4.length
+){;;f4=                                  F4
+;}};;f2                                   [
++f3]=f4
+.concat
+();;;f2
+[(f3)].
+push((
+F3));if
+(f2[f3]
+.length
+>((F2))
+.length
+){F2=(f2)
+[((f3))];}}
+return (F2);};
